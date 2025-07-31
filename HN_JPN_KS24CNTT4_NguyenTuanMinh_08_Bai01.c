4affc79f2629f096b6a246b54cb6c36172de9c1a
Lerent1 <minhlonroi15@gmail.com>
2025-07-31 15:48:30 +0700

    Add files via upload

---
+++ HN_JPN_KS24CNTT4_NguyenTuanMinh_08_Bai01.c
@@ -0,0 +1,211 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 100
+#define Size 10
+
+typedef struct Operation {
+    char url[100];
+    char timestamp[50];
+} Operation;
+
+typedef struct Stack {
+    Operation data[MAX];
+    int top;
+} Stack;
+
+typedef struct History {
+    Operation data;
+    struct History *next;
+    struct History *prev;
+} History;
+
+int initStack(Stack *s) {
+    s->top = -1;
+}
+
+int isEmpty(Stack *s) {
+    return s->top == -1;
+}
+
+int isFull(Stack *s) {
+    return s->top == MAX - 1;
+}
+
+void push(Stack *s, Operation character) {
+    if (!isFull(s)) {
+        s->data[++s->top] = character;
+    }
+}
+
+Operation pop(Stack *s) {
+    Operation empty = {"", ""};
+    if (!isEmpty(s)) {
+        return s->data[s->top--];
+    }
+    return empty;
+}
+
+Operation peek(Stack *s) {
+    Operation empty = {"", ""};
+    if (!isEmpty(s)) {
+        return s->data[s->top];
+    }
+    return empty;
+}
+
+typedef struct Queue {
+    Operation data[MAX];
+    int front;
+    int rear;
+    int size;
+} Queue;
+
+void initQueue(Queue *q) {
+    q->front = q->rear = q->size = 0;
+}
+
+void enqueue(Queue *q, Operation character) {
+    if (q->size == Size) {
+        q->front = (q->front + 1) % Size;
+        q->rear--;
+    }
+    q->data[q->rear] = character;
+    q->rear = (q->rear + 1) % Size;
+    q->size++;
+}
+
+void showQueue(Queue q) {
+    int num = q.size;
+    int fro = q.front;
+    while (num--) {
+        printf("%s\n", q.data[fro].url);
+        printf("%s\n", q.data[fro].timestamp);
+        fro = (fro + 1) % Size;
+    }
+}
+
+void addHistory(History **head, Operation character) {
+    History *newHistory = (History *)malloc(sizeof(History));
+    newHistory->data = character;
+    newHistory->next = NULL;
+    newHistory->prev = NULL;
+    if (*head == NULL) {
+        *head = newHistory;
+    } else {
+        History *nodeHead = head;
+        while (nodeHead->next != NULL) {
+            nodeHead = nodeHead->next;
+            nodeHead->next = newHistory;
+            newHistory->prev = nodeHead;
+        }
+    }
+}
+
+void showHistory(History *head) {
+    History *temp = head;
+    while (temp != NULL) {
+        printf("%s\n", temp->data.url);
+        printf("%s\n", temp->data.timestamp);
+        temp = temp->next;
+    }
+}
+
+void clearHis(History **head) {
+    History *node = *head;
+    while (node != NULL) {
+        History *temp = node->next;
+        free(node);
+        node = temp;
+    }
+}
+
+int main(void) {
+    int choice;
+    char text[100];
+    Operation current = {"", ""};
+
+    Stack back;
+    Stack forword;
+    Queue recent;
+    History *history = NULL;
+
+    initStack(&back);
+    initStack(&forword);
+    initQueue(&recent);
+
+    do {
+        printf("1. Visit url\n");
+        printf("2. Backward\n");
+        printf("3. Forward\n");
+        printf("4. Current\n");
+        printf("5. History\n");
+        printf("6. Clear History\n");
+        printf("7. Exit\n");
+        printf("Lua chon cua ban la: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: {
+                printf("Hay nhap dia chi url: ");
+                scanf("%s", text);
+                getchar();
+                if (strlen(current.url) > 0) {
+                    push(&back, current);
+                }
+                strcpy(current.url, text);
+                initStack(&forword);
+                enqueue(&recent, current);
+                addHistory(&history, current);
+                printf("Da truy cap vao: %s\n", current.url);
+                break;
+            }
+            case 2: {
+                if (!isEmpty(&back)) {
+                    push(&forword, current);
+                    current = pop(&back);
+                    printf("Da truy cap vao: %s\n", current.url);
+                } else {
+                    printf("Ko hop le\n");
+                }
+                break;
+            }
+            case 3: {
+                if (!isEmpty(&forword)) {
+                    push(&back, current);
+                    current = pop(&forword);
+                    printf("Da truy cap vao: %s\n", current.url);
+                } else {
+                    printf("Ko hop le\n");
+                }
+                break;
+            }
+            case 4: {
+                if (strlen(current.url) > 0) {
+                    printf(" trang hien tai: %s\n", current.url);
+                } else {
+                    printf("KO co trang nao\n");
+                }
+                break;
+            }
+            case 5: {
+                showHistory(history);
+                break;
+            }
+            case 6: {
+                clearHis(&history);
+                break;
+            }
+            case 7: {
+                printf("Thoat thanh cong\n");
+                break;
+            }
+            default : {
+                printf("Hay nhap lai\n");
+            }
+        }
+    } while (choice != 7);
+
+    return 0;
+}
---
+++ HN_JPN_KS24CNTT4_NguyenTuanMinh_08_Bai02.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Tree {
+    int id;
+    char name[100];
+    int age;
+    struct Tree* left;
+    struct Tree* right;
+} Tree;
+
+Tree *createTree(int id, char name[], int age) {
+    Tree *tree = (Tree *)malloc(sizeof(Tree));
+    tree->id = id;
+    strcpy(tree->name, name);
+    tree->age = age;
+    tree->left = NULL;
+    tree->right = NULL;
+    return tree;
+}
+
+void addTree(Tree *tree, int id, int age) {
+    if (tree == NULL) {
+    }
+}
+
+void inorder(Tree *tree) {
+    if (tree == NULL) {
+        return;
+    }
+    inorder(tree->left);
+    printf("%s\n", tree->name);
+    inorder(tree->right);
+}
+
+int main(void) {
+    int choice;
+    Tree *root = NULL;
+
+    do {
+        printf("1. Them sinh vien\n");
+        printf("2. Hien thi sinh vien\n");
+        printf("3. Tim kiem sinh vien\n");
+        printf("4. Xoa sinh vien\n");
+        printf("5. Them sinh vien\n");
+        printf("Lua chon cua ban la: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1: {
+                break;
+            }
+            case 2: {
+                printf("Danh sach sinh vien la: ");
+                inorder(root);
+                break;
+            }
+            case 3: {
+                break;
+            }
+            case 4: {
+                break;
+            }
+            case 5: {
+                printf("Thoat thanh cong\n");
+                break;
+            }
+            default: {
+                printf("Hay chon lai\n");
+            }
+        }
+    } while (choice != 5);
+
+    return 0;
+}
